cc05b5ddebc4026492cb86d7e3a1c8511cd9e6e6
UhiriweAudace <u.audace@gmail.com>
2020-09-16 23:43:54 +0200

    HackerRank SortApplication fixed

--- src/App.js
+++ src/App.js
@@ -6,17 +6,27 @@ import Articles from './components/Articles';
 
 const title = "Sorting Articles";
 
-function App({articles}) {
+function App({ articles }) {
+    const [data, setData] = React.useState(articles.sort((a, b) => a.upvotes < b.upvotes ? 1 : -1));
+
+    const onMostUpvote = () => {
+        const mostUpvote = articles.sort((a, b) => a.upvotes < b.upvotes ? 1 : -1);
+        setData([...mostUpvote])
+    }
+    const onMostRecent = () => {
+        const mostRecent = articles.sort((c, d) => new Date(c.date) < new Date(d.date) ? 1 : -1)
+        setData([...mostRecent]);
+    }
 
     return (
         <div className="App">
             <h8k-navbar header={title}></h8k-navbar>
             <div className="layout-row align-items-center justify-content-center my-20 navigation">
                 <label className="form-hint mb-0 text-uppercase font-weight-light">Sort By</label>
-                <button data-testid="most-upvoted-link" className="small">Most Upvoted</button>
-                <button data-testid="most-recent-link" className="small">Most Recent</button>
+                <button data-testid="most-upvoted-link" className="small" onClick={() => onMostUpvote()}>Most Upvoted</button>
+                <button data-testid="most-recent-link" className="small" onClick={() => onMostRecent()}>Most Recent</button>
             </div>
-            <Articles articles={articles}/>
+            <Articles articles={data} />
         </div>
     );
 
